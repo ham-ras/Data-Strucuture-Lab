015b9131ddd0de0767cbef0718b3818f5a3a5e26
HAMRAS MUHAMMAD <67454866+ham-ras@users.noreply.github.com>
2022-01-07 15:44:40 +0530

    Add files via upload

--- bitstring.c
+++ bitstring.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-int U[]={1,2,3,4,5,6,7,8,9}
+//Univeral Set is {1,2,3,4,5,6,7,8,9}
 void input();
 void output();
 void setunion();
@@ -9,10 +9,28 @@ void difference();
 int a[]={0,0,0,0,0,0,0,0,0},b[]={0,0,0,0,0,0,0,0,0};
 int main()
 {
-  int ch,wish;
-  
-  
-
+  int n=0;
+  while(n!=5)
+  {
+   printf("\nBit Operations\n");
+   printf("\nEnter the operation 1.Input 2.Union 3.Intersection 4.Difference 5.Exit \n");
+   scanf("%d",&n);
+   switch(n)
+   {
+    case 1 : input();
+             break;
+    case 2 : setunion();
+             break;
+    case 3 : intersection();
+             break;
+    case 4 : difference();
+             break;
+    case 5 : exit(0);
+             break;
+    default : printf("Invalid input");
+              break;       
+   }
+  }
 }
 
 void input()
@@ -54,7 +72,7 @@ void output(int c[])
  printf("\nSet is ");
  for(i=0;i<9;i++)
  {
-  if(c[i!=0])
+  if(c[i]!=0)
    printf("%d",i+1);
  }  
 }
@@ -88,15 +106,31 @@ void intersection()
   else
    c[i]=0;
  }
- 
+ for(i=0;i<9;i++)
+ {
+  printf("%d",c[i]);
+ }
+ output(c);
 
 }
 
+void difference()
+{
+ int i,c[10];
+ for(i=0;i<9;i++)
+ {
+  if(a[i]==1&&b[i]==0)
+   c[i]=1;
+  else
+   c[i]=0; 
+ }
+ for(i=0;i<9;i++)
+ {
+  printf("%d",c[i]); 
+ }
+ output(c);
 
-
-
-
-
+}
 
 
 
